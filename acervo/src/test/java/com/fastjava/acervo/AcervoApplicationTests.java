fef95eb899a674a71cec87e57577374868aaa26a
Jonathan Alves <jonalves91@gmail.com>
2021-07-15 08:50:11 -0300

    Add files via upload

---
+++ acervo/src/test/java/com/fastjava/acervo/AcervoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastjava.acervo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
