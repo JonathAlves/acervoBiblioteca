cab3ffb11334ff4ab906684ec16be81679c8fee8
Jonathan <jonalves91@gmail.com>
2021-07-16 18:57:34 -0300

    servicos

---
+++ acervo/src/main/java/com/fastjava/acervo/Servicos/AutorServico.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.fastjava.acervo.Servicos;
+
+/**
+ * @author grupo 1
+ *
+ */
+public class AutorServico {
+
+	/**
+	 * 
+	 */
+	public AutorServico() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ acervo/src/main/java/com/fastjava/acervo/Servicos/ObraServico.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.fastjava.acervo.Servicos;
+
+/**
+ * @author grupo 1
+ *
+ */
+public class ObraServico {
+
+	/**
+	 * 
+	 */
+	public ObraServico() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
